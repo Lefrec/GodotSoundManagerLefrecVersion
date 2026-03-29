129c9db70a57b5e5c588c947bce065bb5e2cc86d
Nathan Hoad <nathan@nathanhoad.net>
2026-03-29 12:53:22 +1000

    Update to Godot 4.6 (#40)

--- addons/sound_manager/abstract_audio_player_pool.gd
+++ addons/sound_manager/abstract_audio_player_pool.gd
@@ -1,25 +1,25 @@
 extends Node
 
 
-@export var default_busses := []
-@export var default_pool_size := 8
+@export var default_busses: PackedStringArray = []
+@export var default_pool_size: int = 8
 
 
 var available_players: Array[AudioStreamPlayer] = []
 var busy_players: Array[AudioStreamPlayer] = []
 var bus: String = "Master"
 
-var _tweens: Dictionary = {}
+var _tweens: Dictionary[AudioStreamPlayer, Tween] = {}
 
 
 func _init(possible_busses: PackedStringArray = default_busses, pool_size: int = default_pool_size) -> void:
 	bus = get_possible_bus(possible_busses)
 
-	for i in pool_size:
+	for i: int in pool_size:
 		increase_pool()
 
 func get_possible_bus(possible_busses: PackedStringArray) -> String:
-	for possible_bus in possible_busses:
+	for possible_bus: String in possible_busses:
 		var cases: PackedStringArray = [
 			possible_bus,
 			possible_bus.to_lower(),
@@ -27,7 +27,7 @@ func get_possible_bus(possible_busses: PackedStringArray) -> String:
 			possible_bus.to_pascal_case(),
 			possible_bus.to_snake_case()
 		]
-		for case in cases:
+		for case: String in cases:
 			if AudioServer.get_bus_index(case) > -1:
 				return case
 	return "Master"
@@ -52,20 +52,20 @@ func prepare(resource: AudioStream, override_bus: String = "") -> AudioStreamPla
 func get_available_player() -> AudioStreamPlayer:
 	if available_players.size() == 0:
 		increase_pool()
-	var player = available_players.pop_front()
+	var player: AudioStreamPlayer = available_players.pop_front()
 	busy_players.append(player)
 	return player
 
 
 func get_player_with_resource(resource: AudioStream) -> AudioStreamPlayer:
-	for player in busy_players + available_players:
+	for player: AudioStreamPlayer in busy_players + available_players:
 		if player.stream == resource:
 			return player
 	return null
 
 
 func get_busy_player_with_resource(resource: AudioStream) -> AudioStreamPlayer:
-	for player in busy_players:
+	for player: AudioStreamPlayer in busy_players:
 		if player.stream.resource_path == resource.resource_path:
 			return player
 	return null
@@ -84,13 +84,13 @@ func mark_player_as_available(player: AudioStreamPlayer) -> void:
 
 func increase_pool() -> void:
 	# See if we can reclaim a rogue busy player
-	for player in busy_players:
+	for player: AudioStreamPlayer in busy_players:
 		if not player.playing:
 			mark_player_as_available(player)
 			return
 
 	# Otherwise, add a new player
-	var player := AudioStreamPlayer.new()
+	var player: AudioStreamPlayer = AudioStreamPlayer.new()
 	add_child(player)
 	available_players.append(player)
 	player.bus = bus
@@ -137,7 +137,7 @@ func _on_player_finished(player: AudioStreamPlayer) -> void:
 	mark_player_as_available(player)
 
 
-func _on_fade_completed(player: AudioStreamPlayer, tween: Tween, from_volume: float, to_volume: float, duration: float):
+func _on_fade_completed(player: AudioStreamPlayer, _tween: Tween, _from_volume: float, to_volume: float, _duration: float) -> void:
 	_remove_tween(player)
 
 	# If we just faded out then our player is now available
